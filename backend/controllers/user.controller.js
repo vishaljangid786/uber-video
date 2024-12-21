42cf21d3e15d6da2e46599783b227fa0e6307a7b
vishaljangid786 <vishaljangid80550786@gmail.com>
2024-12-21 09:51:22 +0530

    commited

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -29,3 +29,32 @@ module.exports.registerUser = async (req, res, next) => {
     token,
   });
 };
+
+module.exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  const user = await userService.findUserByEmail(email);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const isMatch = await userModel.comparePassword(password, user.password);
+
+  if (!isMatch) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const token = user.generateAuthToken();
+
+  res.status(200).json({
+    user,
+    token,
+  });
+}
